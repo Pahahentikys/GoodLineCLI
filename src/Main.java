52921c84d3e3003fec09f63c22b8ff856891013e
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-03-07 14:38:51 +0300

    Создание проекта, вывод приветствия.

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Pavel on 07.03.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, i'm your first project!");
+    }
+}
