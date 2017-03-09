60be346f5adbbcbd2cf3aeebf1ca8d4cff11e2e6
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-03-10 02:05:43 +0300

    В класс DataValidator добавлен "сырой" метод получения параметров с командной строки, всё это добро обёрнуто в try-catch...

--- src/DataValidator.java
+++ src/DataValidator.java
@@ -6,12 +6,14 @@ import org.apache.commons.cli.*;
 
 public class DataValidator {
     private static CommandLine commandLine;
-    private static Options options = new Options();
+    Options options = new Options();
+    UserInputData userInputData = new UserInputData();
     CommandLineParser commandLineParser = new DefaultParser();
 
 
     public Options generateOptions() {
-        options.addOption("h", "help", true, "Показать справочную информацию!");
+        Options options = new Options();
+        options.addOption("h", "help", true, "Показать справочную информацию");
         options.addOption("login", "login", true, "Логин: ");
         options.addOption("pass", "pass", true, "Пароль: ");
         options.addOption("res", "res", true, "Ресурс для доступа: ");
@@ -22,5 +24,25 @@ public class DataValidator {
         return options;
     }
 
+    public UserInputData getUserInputData(String[] args) {
+        try {
+            userInputData = new UserInputData();
+            commandLine = commandLineParser.parse(options, args);
+            userInputData.setUserInputLogin(commandLine.getOptionValue("login"));
+            userInputData.setUserInputPassword(commandLine.getOptionValue("pass"));
+            userInputData.setUserInputPathResource(commandLine.getOptionValue("res"));
+            userInputData.setUserInputRole(commandLine.getOptionValue("role"));
+            userInputData.setUserInputDs(commandLine.getOptionValue("ds"));
+            userInputData.setUserInputDe(commandLine.getOptionValue("de"));
+            userInputData.setUserInputVol(commandLine.getOptionValue("vol"));
+
+        } catch (ParseException ex) {
+            HelpFormatter helpFormatter = new HelpFormatter();
+            helpFormatter.printHelp("Показать справочную информацию: ", options);
+            System.exit(0);
+        }
+
+        return null;
+    }
 
 }
