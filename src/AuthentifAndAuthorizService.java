54ab3b2e3646bd631108d3a1a4cd923724be6f62
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-03-13 19:43:03 +0300

    В класс: AAAService добавлен метод: isGetUserPassword, который позволяет сравнить хэш пароля введённый пользователем с хэшем из коллекции пользователя, который заранее задан.

--- src/AuthentifAndAuthorizService.java
+++ src/AuthentifAndAuthorizService.java
@@ -14,7 +14,7 @@ public class AuthentifAndAuthorizService {
      *
      * @param usersList   - коллекция пользователей
      * @param userInpdata - входные данные
-     * @return
+     * @return - true, если логины совпадают
      */
     private boolean isGetUserLogin(ArrayList<UserInfo> usersList, UserInputData userInpdata) {
         for (UserInfo user : usersList) {
@@ -29,12 +29,33 @@ public class AuthentifAndAuthorizService {
 
     /**
      * Сравнить хэши на совпадение
-     * @param user - информация о пользователе, который задан, из коллекции
+     *
+     * @param user         - информация о пользователе, который задан, из коллекции
      * @param hashUserPass - хэш пароля, который ввёл пользователь
      * @return
      */
     private boolean isUserHashesEqual(UserInfo user, String hashUserPass) {
         return user.getUserHashPassword().equals(hashUserPass);
+    }
+
+    /**
+     * Получить пароль пользователяя
+     *
+     * @param usersList   - коллекция пользователей
+     * @param userInpdata - введённые пользователем данные
+     * @return - true, если парольные хэши совпадают
+     */
+    private boolean isGetUserPassword(ArrayList<UserInfo> usersList, UserInputData userInpdata) {
+
+        for (UserInfo user : usersList) {
+            if (userInpdata.getUserInputId() == user.getUserId()) {
+                String hashUserPass = generHashUserPassword(userInpdata.getUserInputPassword(), user.getUserSalt());
+                if (isUserHashesEqual(user, hashUserPass)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     /**
