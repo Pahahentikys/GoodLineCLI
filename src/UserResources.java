59475d1b595f4a2b588f853e77af8a6989d537f5
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-03-15 22:48:17 +0300

    Коллекция пользователей и ресурсов была вынесена из класса: UserResources в Main

--- src/UserResources.java
+++ src/UserResources.java
@@ -57,34 +57,34 @@ public class UserResources {
         return this;
     }
 
-    /**
-     * Коллекция объектов ресурсов, на которые может переходить пользователь
-     */
-    private static List<UserResources> usersResources = Arrays.asList(
-            new UserResources()
-                    .setUserResId(1)
-                    .setUserResUserId(1)
-                    .setUserResResId(1)
-                    .setResourcePath("A.B")
-                    .setUserRole(UserRoles.EXECUTE)
-            ,
-            new UserResources()
-                    .setUserResId(2)
-                    .setUserResUserId(2)
-                    .setUserResResId(2)
-                    .setResourcePath("A.B.C")
-                    .setUserRole(UserRoles.EXECUTE)
-
-    );
-
-    /**
-     * Получить информацию о ресурсе и его пользователе
-     *
-     * @return - коллекция объектов с ресурами и пользователями
-     */
-    public static List<UserResources> getUsersResources() {
-        return usersResources;
-    }
+//    /**
+//     * Коллекция объектов ресурсов, на которые может переходить пользователь
+//     */
+//    private static List<UserResources> usersResources = Arrays.asList(
+//            new UserResources()
+//                    .setUserResId(1)
+//                    .setUserResUserId(1)
+//                    .setUserResResId(1)
+//                    .setResourcePath("A.B")
+//                    .setUserRole(UserRoles.EXECUTE)
+//            ,
+//            new UserResources()
+//                    .setUserResId(2)
+//                    .setUserResUserId(2)
+//                    .setUserResResId(2)
+//                    .setResourcePath("A.B.C")
+//                    .setUserRole(UserRoles.EXECUTE)
+//
+//    );
+//
+//    /**
+//     * Получить информацию о ресурсе и его пользователе
+//     *
+//     * @return - коллекция объектов с ресурами и пользователями
+//     */
+//    public static List<UserResources> getUsersResources() {
+//        return usersResources;
+//    }
 
 
 }
