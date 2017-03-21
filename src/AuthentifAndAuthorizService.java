175281f3c10e65903c968d8c9c9c13b3e7066f3e
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-03-21 03:37:28 +0300

    Пункт: 2, Release 1.0 #1. Правка в конструкиях с логическими операторами & и &&. Разница между ними состоит в том, что в случае с одинарным - &, в конструкции с множеством операндов и его использовании  проверится только один операнд. То есть, если найдено false, то всё выражение уйдёт на false.

--- src/AuthentifAndAuthorizService.java
+++ src/AuthentifAndAuthorizService.java
@@ -113,7 +113,7 @@ public class AuthentifAndAuthorizService {
      */
 
     public static boolean isUserAuthorization(ArrayList<UserResources> resourcesList, UserInputData userInpData, boolean isUserAuthentification) {
-        if (isUserAuthentification & userInpData.getUserInputRole() != null & userInpData.getUserInputPathResource() != null) {
+        if (isUserAuthentification && userInpData.getUserInputRole() != null && userInpData.getUserInputPathResource() != null) {
             if (!DataValidator.isUserRoleValid(userInpData)) {
                 System.exit(3);
             }
@@ -150,7 +150,7 @@ public class AuthentifAndAuthorizService {
      * @return - код: 5, если некорректная дата
      */
     public static boolean isUserAccounting(ArrayList<Accounting> accountingList, UserInputData userInputData, boolean isUserAuthorization) {
-        if (isUserAuthorization & userInputData.getUserInputDs() != null) {
+        if (isUserAuthorization && userInputData.getUserInputDs() != null) {
             if (!DataValidator.isDateDsAndDeValid(userInputData) || !DataValidator.isVolumeValid(userInputData)) {
                 System.exit(5);
             }
