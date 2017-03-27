2907323b91159570002a58de88040723af932dad
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-03-27 19:35:10 +0300

    Обновил баш-скрипт для компиляции файлов проекта и сборки их в jar-архив, добавил в него вопрос для запуска, прописал пути до библиотек.

--- BUILD.sh
+++ BUILD.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+read -n 1 -p "Запустить скрипт для построения билда? (y/n): " AMSURE
+[ "$AMSURE" = "y" ] || exit
+echo "" 1>&2
+# Создать папку: out
+mkdir -p out
 
-# Создать папку: bin
-mkdir bin
-
-# Хранить файлы в отдельной папке: bin, не мешать с исходниками
-javac -d bin src/Main.java -cp "libs/*.jar"
+# Хранить файлы в отдельной папке: out, не мешать с исходниками
+javac -encoding utf8 -d ./out/ -sourcepath src/ ./src/Main.java -classpath "./libs/commons-codec-1.10.jar;./libs/commons-lang3-3.5.jar;./libs/commons-cli-1.3.1.jar"
 
 #Упаковать файлы в jar-архив
-jar cfm GoodLineCLI.jar META-INF/MANIFEST.MF bin/
+jar cvmf META-INF/MANIFEST.MF GoodLineCLI.jar out/
