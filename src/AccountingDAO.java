a774631b9c7d3347be008ac8832148daf9fe3b70
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-04-10 18:33:13 +0300

    Реализован метод: addUserSeans() для фиксации пользовательского сеанса в классе: AccountingDAO

--- src/AccountingDAO.java
+++ src/AccountingDAO.java
@@ -1,4 +1,4 @@
-import java.sql.Connection;
+import java.sql.*;
 
 /**
  * Created by Pavel on 10.04.2017.
@@ -10,8 +10,32 @@ public class AccountingDAO {
         this.connection = connection;
     }
 
-    private void addUserSeans(Accounting accounting){
+    private void addUserSeans(Accounting accounting) {
 
+        try {
+
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO GOOD_LINE_CLI_SCHEME.USER_SEANS(user_resource_id, user_seans_date_start, user_seans_date_end, user_seans_volume) VALUES (?, ?, ?, ?)");
+
+            preparedStatement.setInt(1, accounting.getResourceUserId());
+
+            preparedStatement.setDate(2, Date.valueOf(accounting.getStartAccountingDate()));
+
+            preparedStatement.setDate(3, Date.valueOf(accounting.getEndAccountingDate()));
+
+            preparedStatement.setInt(4, Integer.parseInt(accounting.getVolumeOfUseRes()));
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            System.out.println("Сессия сохранена!" + resultSet.getInt("user_seans_id") +
+                    resultSet.getInt("user_resource_id") +
+                    resultSet.getDate("user_seans_date_start") +
+                    resultSet.getDate("user_seans_date_end") +
+                    resultSet.getInt("user_seans_volume")
+            );
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 
