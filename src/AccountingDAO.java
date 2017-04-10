7cf915f9d582a8b10f8470ad80de5953ba5ba7ee
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-04-10 18:03:27 +0300

    Создан класс: AccountingDAO для фиксации пользовательского сеанса в базе, поправлен план п. 5, изменил наименование класса

---
+++ src/AccountingDAO.java
@@ -0,0 +1,18 @@
+import java.sql.Connection;
+
+/**
+ * Created by Pavel on 10.04.2017.
+ */
+public class AccountingDAO {
+    private Connection connection;
+
+    AccountingDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    private void addUserSeans(Accounting accounting){
+
+    }
+
+
+}
