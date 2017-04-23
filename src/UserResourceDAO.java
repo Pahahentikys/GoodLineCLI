7347ff5db9b3102e3dac085d0fe0d5596bd42677
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-04-23 04:26:31 +0300

    В класс: UserResourceDAO, помещён метод для поиска ресурса: getPathUserResource.

--- src/UserResourceDAO.java
+++ src/UserResourceDAO.java
@@ -1,4 +1,8 @@
+import javax.management.relation.Role;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * Created by Pavel on 23.04.2017.
@@ -9,5 +13,31 @@ public class UserResourceDAO {
 
     public UserResourceDAO(Connection connection) {
         this.connection = connection;
+    }
+
+    static final String selectUserResourcesWherePath = "SELECT * FROM GOOD_LINE_CLI_SCHEME.USER_RESOURCES WHERE" +
+            "(USER_RESOURCES.USER_RESOURCE_PATH = userResourcePath) OR (USER_RESOURCES.USER_RESOURCE_PATH LIKE ?) AND " +
+            "(USER_RESOURCES.USER_RESOURCE_ROLE LIKE ?)";
+
+    UserResources getPathUserResource(int userInfoId, String userResourcePath, String userRole) throws SQLException {
+
+        try {
+            PreparedStatement statement = connection.prepareStatement(selectUserResourcesWherePath);
+            System.out.println("Ресурс: " + userResourcePath);
+            statement.setInt(1, userInfoId);
+            statement.setString(2, userResourcePath);
+            statement.setString(3, userRole);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+
+                System.out.println("ID: " + resultSet.getInt("USER_ID") + ", " + resultSet.getString("USER_RESOURCE_PATH")
+                        + ", " + resultSet.getString("USER_RESOURCE_ROLE"));
+            }
+
+            } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
