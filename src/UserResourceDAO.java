6983e4f6a0990ebc2bbbf806536cba124871590e
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-05-18 16:15:14 +0300

    В классе: UserResourceDAO убрал мусор.

--- src/UserResourceDAO.java
+++ src/UserResourceDAO.java
@@ -9,36 +9,31 @@ import java.sql.SQLException;
  */
 public class UserResourceDAO {
 
-    UserInputData ui;
     Connection connection;
 
     public UserResourceDAO(Connection connection) {
         this.connection = connection;
     }
 
-    final String selectUserResourcesWherePath = "SELECT * FROM GOOD_LINE_CLI_SCHEME.USER_RESOURCES WHERE" +
-            "(USER_RESOURCES.USER_ID LIKE ?) AND "+
-            "(USER_RESOURCES.USER_RESOURCE_PATH LIKE ?) AND " +
+final String selectUserResourcesWherePath = "SELECT * FROM GOOD_LINE_CLI_SCHEME.USER_RESOURCES WHERE" +
+            "(USER_RESOURCES.USER_RESOURCE_PATH LIKE ?) AND "+
             "(USER_RESOURCES.USER_RESOURCE_ROLE LIKE ?)";
 
-    UserResources getPathUserResource(int userInfoId, String userResourcePath, String userRole) throws SQLException {
+    UserResources getPathUserResource(String userResourcePath, String userRole) throws SQLException {
 
         try {
             PreparedStatement statement = connection.prepareStatement(selectUserResourcesWherePath);
-            System.out.println("Ресурс: " + userInfoId + ", " + userResourcePath + ", " + userRole);
-            statement.setInt(1, userInfoId);
-            statement.setString(2, userResourcePath);
-            statement.setString(3, userRole);
+            System.out.println(userResourcePath + ", " + userRole);
+            statement.setString(1, userResourcePath);
+            statement.setString(2, userRole);
             ResultSet resultSet = statement.executeQuery();
 
             if (resultSet.next()) {
 
-                System.out.println("ID: " + resultSet.getInt("USER_ID") + ", " + resultSet.getString("USER_RESOURCE_PATH")
+                System.out.println(resultSet.getString("USER_RESOURCE_PATH")
                         + ", " + resultSet.getString("USER_RESOURCE_ROLE"));
                 return new UserResources()
-                        .setUserResUserId(resultSet.getInt("USER_ID"))
                         .setResourcePath(resultSet.getString("USER_RESOURCE_PATH"));
-
             }
 
         } catch (SQLException e) {
