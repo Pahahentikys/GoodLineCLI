8bac7d96f23a1030862380183016128df9dd944a
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-05-20 14:16:31 +0300

    Уборка в классе: AccountingDAO. Изменил executeQuery на executeUpdate.

--- src/AccountingDAO.java
+++ src/AccountingDAO.java
@@ -1,4 +1,7 @@
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /**
  * Created by Pavel on 10.04.2017.
@@ -10,33 +13,24 @@ public class AccountingDAO {
         this.connection = connection;
     }
 
-    private void addUserSeans(Accounting accounting) {
+    public void addUserSeans(Accounting accounting) {
 
         try {
-
             PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO GOOD_LINE_CLI_SCHEME.USER_SEANS(user_resource_id, user_seans_date_start, user_seans_date_end, user_seans_volume) VALUES (?, ?, ?, ?)");
 
-            preparedStatement.setInt(1, accounting.getResourceUserId());
+            preparedStatement.setInt(1, accounting.getResourceId());
 
             preparedStatement.setDate(2, Date.valueOf(accounting.getStartAccountingDate()));
 
             preparedStatement.setDate(3, Date.valueOf(accounting.getEndAccountingDate()));
 
-            preparedStatement.setInt(4, Integer.parseInt(accounting.getVolumeOfUseRes()));
+            preparedStatement.setInt(4, Integer.parseInt(String.valueOf(accounting.getVolumeOfUseRes())));
 
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            System.out.println("Сессия сохранена!" + resultSet.getInt("user_seans_id") +
-                    resultSet.getInt("user_resource_id") +
-                    resultSet.getDate("user_seans_date_start") +
-                    resultSet.getDate("user_seans_date_end") +
-                    resultSet.getInt("user_seans_volume")
-            );
+            preparedStatement.executeUpdate();
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
-
 
 }
