ac481063a754536c84e7ff5f58c40f68fb23dd98
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-05-21 21:33:05 +0300

    Поправил файл: BUILD.sh под текущую структуру проекта

--- BUILD.sh
+++ BUILD.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Создать папку: out
 mkdir -p out
-
 # Хранить файлы в отдельной папке: out, не мешать с исходниками
-javac -encoding utf8 -d ./out/ -sourcepath src/ ./src/Main.java -classpath "./libs/commons-codec-1.10.jar;./libs/commons-lang3-3.5.jar;./libs/commons-cli-1.3.1.jar"
-
+cp -R -v ./src/main/resources/* ./out
+javac -encoding utf8 -d "./out" -sourcepath ./src/main/java/ ./src/main/java/general/Main.java -classpath "./libs/commons-codec-1.10.jar;./libs/commons-lang3-3.5.jar;./libs/commons-cli-1.3.1.jar;./libs/h2-1.4.194.jar;./libs/flyway-core-4.1.2.jar;./libs/log4j-core-2.8.2.jar;./libs/log4j-api-2.8.2.jar"
+#main/java/
 #Упаковать файлы в jar-архив
-jar cvmf META-INF/MANIFEST.MF GoodLineCLI.jar out/
+jar cvmf META-INF/MANIFEST.MF GoodLineCLI.jar -C out/ .
