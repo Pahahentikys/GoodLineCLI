11f399da7468b53c58c28bbaa5d9cc7ff5c77df9
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-10-12 15:32:32 +0300

    Работа над R4. Работа над тестами. В класс: AuthentificationService добавлен метод: setDataAboutUser, который будет создавать при тесте экземпляр класса AutService и подкидывать в него определённый объект.

--- src/test/java/AuthentificationServiceTest.java
+++ src/test/java/AuthentificationServiceTest.java
@@ -1,7 +1,30 @@
-import general.dom.UserInfo;
 import general.dao.UserInfoDAO;
+import general.dom.UserInfo;
+import general.serv.AuthenticationService;
 import org.junit.Before;
-import org.junit.Test;
+import org.mockito.Mockito;
+
 
 public class AuthentificationServiceTest {
+
+    // Сервис, ответственный за аутентификацию юзера.
+    private AuthenticationService authenticationService;
+
+    @Before
+    public void setDataAboutUser() {
+
+        // Класс для доступа к данным в БД
+        UserInfoDAO userInfoDAO = Mockito.mock(UserInfoDAO.class);
+
+        // Дефолтный юзер, чтобы его мокнуть
+        UserInfo userInfo = new UserInfo()
+                .withUserLogin("jdoe")
+                .withUserHashPassword("d824656e5bb0902512e1e8a7f1a099cb")
+                .withUserSalt("+NSw]7");
+
+        // Сюда будет подставляеться юзер через метод в классе AuthentificationService
+        authenticationService = new AuthenticationService();
+
+    }
+
 }
