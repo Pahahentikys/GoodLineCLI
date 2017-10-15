d794b82f40a3293645c81269774f4d3156fcfa1d
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-10-16 02:06:42 +0700

    R4. В классе: AuthentificationServiceTest произведён рефакторинг и доработка предшествующего кода, написан набор методов для тестирования механизма аутентификации: 1. setDefaultParamsForTests - задаёт базовый объект userInfo для сравнения с тестовыми данными, задаёт Mock для UserInfoDAO, также набор Mock-ситуации через when. 2. testInvalidLogin - поведение программы при некорректном логине. 3. testInvalidPassword - поведение программы при некорректном пароле. 4. testValidLoginAndPassword - поведение программы при корректном логине и пароле.

--- src/test/java/AuthentificationServiceTest.java
+++ src/test/java/AuthentificationServiceTest.java
@@ -1,3 +1,4 @@
+import general.ExitCodeType;
 import general.dao.UserInfoDAO;
 import general.dom.UserInfo;
 import general.dom.UserInputData;
@@ -17,14 +18,14 @@ public class AuthentificationServiceTest {
     // Сервис, ответственный за аутентификацию юзера.
     private AuthenticationService authenticationService;
 
+    private UserInfoDAO userInfoDAO;
+
+    // Стоп-код при выолнении программы
+    private int exitCode;
+
     @Before
-    public void setDataAboutUser() {
-
+    public void setDefaultParamsForTests() throws SQLException {
         authenticationService = new AuthenticationService();
-    }
-
-    @Test
-    public void testLoginValid() throws SQLException {
 
         // Дефолтный юзер, чтобы его мокнуть
         UserInfo userInfo = new UserInfo()
@@ -32,21 +33,50 @@ public class AuthentificationServiceTest {
                 .withUserHashPassword("d824656e5bb0902512e1e8a7f1a099cb")
                 .withUserSalt("+NSw]7");
 
-        String userLogin = userInfo.getUserLogin();
-        String userPass = userInfo.getUserHashPassword();
-
-        UserInputData userInputData = new UserInputData("jdoe", "sup3rpaZZ");
-
-        // Класс для доступа к данным в БД
-        UserInfoDAO userInfoDAO = Mockito.mock(UserInfoDAO.class);
+        userInfoDAO = Mockito.mock(UserInfoDAO.class);
 
         // Mock на ситуацию, когда пользователь с таким логином найден в БД
         when(userInfoDAO.searchUserLogin("jdoe")).thenReturn(userInfo);
 
         // Mock на ситуацию, когда пользователь с таким логином найден в БД
         when(userInfoDAO.searchUserLogin("xxx")).thenReturn(null);
-
-        assertEquals(true, authenticationService.isUserAuthentification(userInfoDAO, userLogin, userPass));
     }
 
+    @Test
+    public void testInvalidLogin() throws SQLException {
+
+        UserInputData userInputData = new UserInputData("xxx", "sup3rpaZZ");
+        String userLogin = userInputData.getUserInputLogin();
+        String userPass = userInputData.getUserInputPassword();
+
+        exitCode = authenticationService.isUserAuthentification(userInfoDAO, userLogin,
+                userPass);
+
+        assertEquals(exitCode, ExitCodeType.INVALID_LOGIN.getExitCode());
+    }
+
+    @Test
+    public void testInvalidPassword() throws SQLException {
+
+        UserInputData userInputData = new UserInputData("jdoe", "xxx");
+        String userLogin = userInputData.getUserInputLogin();
+        String userPass = userInputData.getUserInputPassword();
+
+        exitCode = authenticationService.isUserAuthentification(userInfoDAO, userLogin,
+                userPass);
+
+        assertEquals(exitCode, ExitCodeType.INVALID_PASSWORD.getExitCode());
+    }
+
+    @Test
+    public void testValidLoginAndPassword() throws SQLException {
+        UserInputData userInputData = new UserInputData("jdoe", "sup3rpaZZ");
+        String userLogin = userInputData.getUserInputLogin();
+        String userPass = userInputData.getUserInputPassword();
+
+        exitCode = authenticationService.isUserAuthentification(userInfoDAO, userLogin,
+                userPass);
+
+        assertEquals(exitCode, ExitCodeType.SUCCESS.getExitCode());
+    }
 }
