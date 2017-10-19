243e92b8ece9c4ad4fc2d3818ca2d3b58dec29c3
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-10-19 12:25:48 +0700

    R4. Работа над тестированием авторизации пользователя. Дополнен класс тестирования авторизации пользователя: 1. Добавлен метод: testValidAuthorizationToResA(). 2. Добавлено поле userAuthCode, которое фиксирует успешность/неуспешность аутентификации. 3. Поправлены параметры ресурсов в when. 4. Добавлен mock на ситуацию, когда найден пользователь по логину: jdoe.

--- src/test/java/AuthorizationServiceTest.java
+++ src/test/java/AuthorizationServiceTest.java
@@ -1,19 +1,27 @@
+import general.ExitCodeType;
 import general.dao.UserInfoDAO;
 import general.dao.UserResourceDAO;
 import general.dom.UserInfo;
+import general.dom.UserInputData;
 import general.dom.UserResources;
+import general.serv.AuthenticationService;
 import general.serv.AuthorizationService;
 import org.junit.Before;
+import org.junit.Test;
 import org.mockito.Mockito;
 
 import java.sql.SQLException;
 
+import static junit.framework.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
 /**
  * Created by Pavel on 17.10.2017.
  */
 public class AuthorizationServiceTest {
+
+    // Сервис, ответственный за аутентификацию пользователя.
+    private AuthenticationService authenticationService;
 
     // Сервис, ответственный за авторизацию пользователя.
     private AuthorizationService authorizationService;
@@ -27,10 +35,16 @@ public class AuthorizationServiceTest {
     // Стоп-код при выолнении программы.
     private int exitCode;
 
+    // Стоп-код успешности/неуспешности выполнения аутентификации.
+    private int userAuthCode;
+
+
     @Before
     public void setDefaultParamsForTests() throws SQLException {
 
         authorizationService = new AuthorizationService();
+
+        authenticationService = new AuthenticationService();
 
         // Дефолтный юзер, чтобы его мокнуть
         UserInfo userInfo = new UserInfo()
@@ -40,7 +54,7 @@ public class AuthorizationServiceTest {
 
         // Дефолтный ресурсы для дальнейшего мока.
         UserResources userResourceA = new UserResources()
-            .setResourcePath("A");
+                .setResourcePath("A");
 
         // Дефолтный ресурсы для дальнейшего мока.
         UserResources userResourceAAndB = new UserResources()
@@ -50,13 +64,34 @@ public class AuthorizationServiceTest {
         userInfoDAO = Mockito.mock(UserInfoDAO.class);
         userResourceDAO = Mockito.mock(UserResourceDAO.class);
 
-        // Mock на ситуацию, когда пользователь с правами доступа на ресурс найден в БД.
-        when(userResourceDAO.getPathUserResource("A", "READ" )).thenReturn(userResourceA);
+        // Mock на ситуацию, когда пользователь с таким логином найден в БД
+        when(userInfoDAO.searchUserLogin("jdoe")).thenReturn(userInfo);
 
-        // Mock на ситуацию, когда пользователь с правами доступа на ресурс не найден в БД.
-        when(userResourceDAO.getPathUserResource("A.B", "READ")).thenReturn(userResourceAAndB);
+        // Mock на ситуацию, когда пользователь с правами доступа на ресурс найден в БД.
+        when(userResourceDAO.getPathUserResource("a", "READ")).thenReturn(userResourceA);
+
+        // Mock на ситуацию, когда пользователь с правами доступа на ресурс найден в БД.
+        when(userResourceDAO.getPathUserResource("a.b", "READ")).thenReturn(userResourceAAndB);
 
         // Mock на ситуацию, когда задан некорректный ресурс для доступа пользователя.
-        when(userResourceDAO.getPathUserResource("A.B", "xxx")).thenReturn(userResourceAAndB);
+        when(userResourceDAO.getPathUserResource("a.b", "xxx")).thenReturn(userResourceAAndB);
+    }
+
+    @Test
+    public void testValidAuthorizationToResA() throws SQLException {
+
+        UserInputData userInputData = new UserInputData("jdoe", "sup3rpaZZ", "READ", "a");
+        String userLogin = userInputData.getUserInputLogin();
+        String userPass = userInputData.getUserInputPassword();
+        String userRole = userInputData.getUserInputRole();
+        String userPath = userInputData.getUserInputPathResource();
+
+        userAuthCode = authenticationService.isUserAuthentification(userInfoDAO, userLogin,
+                userPass);
+
+        exitCode = authorizationService.isUserAuthorization(userResourceDAO, userPath, userRole, userAuthCode);
+
+
+        assertEquals(exitCode, ExitCodeType.SUCCESS.getExitCode());
     }
 }
