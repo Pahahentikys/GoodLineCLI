a35574bf2cb9550a7221dbcc158d40a680138596
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-10-20 08:09:30 +0300

    Работа над R4. Тест на вывод справки. Добавлены методы: 1. testHelpMenu() - тестирование вывода справки с ключом -h. 2. testHelpMenuWithNoParams() - тестирование вывода справки без пердачи параметров.

--- src/test/java/HelpMenuTest.java
+++ src/test/java/HelpMenuTest.java
@@ -1,5 +1,41 @@
+import general.dom.UserInputData;
+import general.serv.AuthenticationService;
+import general.serv.DataValidator;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by Pavel on 19.10.2017.
  */
 public class HelpMenuTest {
+
+    private UserInputData userInputData;
+
+    private DataValidator dataValidator;
+
+    private AuthenticationService authenticationService;
+
+
+    @Before
+    public void setDefaultParamsForTests() {
+
+        userInputData = new UserInputData();
+
+        dataValidator = new DataValidator();
+
+        authenticationService = new AuthenticationService();
+    }
+
+    @Test
+    public void testHelpMenu() {
+
+        dataValidator.getUserInputData(userInputData, new String[]{"-h"});
+
+    }
+
+    @Test
+    public void testHelpMenuWithNoParams() {
+
+        dataValidator.getUserInputData(userInputData, new String[0]);
+    }
 }
