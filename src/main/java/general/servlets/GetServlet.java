ea58526fe49db1147d83d07e5daf50604ae1bdbc
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-10-25 01:11:13 +0300

    Работа над R5.  В плане отмечены пункты 5. 1. 1, 5. 1. 1. 1. В классе: GetServlet переопределён метод doGet, внутри которого реализовано получение некоторого Id.

--- src/main/java/general/servlets/GetServlet.java
+++ src/main/java/general/servlets/GetServlet.java
@@ -1,7 +1,28 @@
 package general.servlets;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  * Created by Pavel on 25.10.2017.
  */
-public class GetServlet {
+public class GetServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        // В данный объект будет помешаться некоторая html-структура.
+        PrintWriter printerWriter = resp.getWriter();
+
+        // Отрисовка необходимой html-структуры по обращению к данному сервлету.
+        printerWriter.println("<html>");
+        printerWriter.println("<body>");
+        printerWriter.println("<h1>ID: " + req.getParameter("id") + "</h1>");
+        printerWriter.println("</body>");
+        printerWriter.println("</html>");
+    }
 }
