c6d44f1e0bf29c28219547a9a826fb0aa57f40db
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-10-27 15:49:43 +0700

    Работа над R5. Работа на п. 5. 1. 1. 2.  GetServlet переделан на работу с JSP-страницами.

--- src/main/java/general/servlets/GetServlet.java
+++ src/main/java/general/servlets/GetServlet.java
@@ -5,7 +5,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
  * Created by Pavel on 25.10.2017.
@@ -15,14 +14,7 @@ public class GetServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        // В данный объект будет помешаться некоторая html-структура.
-        PrintWriter printerWriter = resp.getWriter();
-
-        // Отрисовка необходимой html-структуры по обращению к данному сервлету.
-        printerWriter.println("<html>");
-        printerWriter.println("<body>");
-        printerWriter.println("<h1>ID: " + req.getParameter("id") + "</h1>");
-        printerWriter.println("</body>");
-        printerWriter.println("</html>");
+        // Отрисовка html-страницы при помощи jsp
+        req.getRequestDispatcher("/getServletPage.jsp").forward(req, resp);
     }
 }
