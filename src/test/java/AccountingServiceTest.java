39825635d2c840ceae317798b5dd357d8dd98c18
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-11-03 03:10:50 +0700

    Работа над R4. Issue #3, работа над п. 1, п. 2, п. 3. В классе: AuthorizationServiceTest тестируемый функционал раздроблен на меньшие методы.

--- src/test/java/AccountingServiceTest.java
+++ src/test/java/AccountingServiceTest.java
@@ -18,157 +18,49 @@ import static org.mockito.Mockito.when;
 public class AccountingServiceTest {
 
     // Класс, отвечающий за валидацию дат и объёма.
-    private DataValidator dataValidator = new DataValidator();
-
-    // Класс, фиксирующий состояние сеанса юзера.
-    private Accounting accounting = new Accounting();
-
-    // Сервис, ответственный за авторизацию пользователя.
-    private AuthorizationService authorizationService;
-
-    // Слой данных, содержащий данные о пользователе.
-    private UserInfoDAO userInfoDAO;
-
-    // Слой данных, содержащий ресурсы пользователя.
-    private UserResourceDAO userResourceDAO;
-
-    // Стоп-код при выолнении программы.
-    private int exitCode;
-
-    // Стоп-код успешности/неуспешности выполнения аутентификации.
-    private int userAuthCode;
-
-    // Стоп-код успешности/неуспешности выполнения авторизации.
-    private int userAuthorizationCode;
+    private DataValidator dataValidator;
 
     @Before
     public void setDefaultParamsForTests() throws SQLException {
 
-        // Дефолтный юзер, чтобы его мокнуть
-        UserInfo userInfo = new UserInfo()
-                .withUserLogin("jdoe")
-                .withUserHashPassword("d824656e5bb0902512e1e8a7f1a099cb")
-                .withUserSalt("+NSw]7");
-
-        // Дефолтный ресурс для дальнейшего мока.
-        UserResources userResourceAAndB = new UserResources()
-                .withUserRole(UserRoles.READ)
-                .withUserResResId(1)
-                .setResourcePath("a.b");
-
-        // Мокаем группу слоёв доступа к данным.
-        userInfoDAO = Mockito.mock(UserInfoDAO.class);
-        userResourceDAO = Mockito.mock(UserResourceDAO.class);
-        AccountingDAO accountingDAO = Mockito.mock(AccountingDAO.class);
-
-
-        // Mock на ситуацию, когда пользователь с таким логином найден в БД
-        when(userInfoDAO.searchUserLogin("jdoe")).thenReturn(userInfo);
-
-        when(userResourceDAO.findIdRes(userResourceAAndB.getResourcePath())).thenReturn(userResourceAAndB);
-
-        when(userResourceDAO.getPathUserResource("a.b", "READ")).thenReturn(userResourceAAndB);
+        dataValidator = new DataValidator();
     }
 
     @Test
-    public void accountinTestValid() throws SQLException {
-
-        accounting = new Accounting();
-
-        UserInputData userInputData = new UserInputData(
-                "jdoe",
-                "sup3rpaZZ",
-                "a.b",
-                "READ",
-                "2017-10-19",
-                "2017-10-19",
-                "100"
-        );
-
-        authentificateUser();
-
-        authorizeUser();
-
-        exitCode = authorizationService.isUserAccounting(accounting, userResourceDAO, userInputData,
-                dataValidator, userAuthorizationCode);
-
-        assertEquals(exitCode, ExitCodeType.SUCCESS.getExitCode());
-    }
-
-    @Test
-    public void accountinTestInvalidDate() throws SQLException {
-
-        UserInputData userInputData = new UserInputData(
-                "xxx-10-19",
-                "2017-10-19",
-                "100"
-        );
-
-        authentificateUser();
-
-        authorizeUser();
-
-        exitCode = authorizationService.isUserAccounting(accounting, userResourceDAO, userInputData,
-                dataValidator, userAuthorizationCode);
-
-        assertEquals(exitCode, ExitCodeType.INVALID_ACTION.getExitCode());
-    }
-
-    @Test
-    public void accountinTestInvalidVolume() throws SQLException {
-
-        UserInputData userInputData = new UserInputData(
-                "2017-10-19",
-                "2017-10-19",
-                "xxx"
-        );
-
-        authentificateUser();
-
-        authorizeUser();
-
-        exitCode = authorizationService.isUserAccounting(accounting, userResourceDAO, userInputData,
-                dataValidator, userAuthorizationCode);
-
-        assertEquals(exitCode, ExitCodeType.INVALID_ACTION.getExitCode());
-    }
-
-    private int authentificateUser() throws SQLException {
-
-        AuthenticationService authenticationService = new AuthenticationService();
+    public void accountingTestInvalidDate() throws SQLException {
 
         UserInputData userInputData = new UserInputData()
-                .withUserInputLogin("jdoe")
-                .withUserInputPassword("sup3rpaZZ");
-        String userLogin = userInputData.getUserInputLogin();
-        String userPass = userInputData.getUserInputPassword();
+                .withUserInputDs("xxx-10-19")
+                .withUserInputDe("2017-10-19");
 
-
-        if(authenticationService.isUserAuthentification(userInfoDAO, userLogin,
-                userPass) == ExitCodeType.SUCCESS.getExitCode()){
-
-            return userAuthCode = ExitCodeType.SUCCESS.getExitCode();
-        }
-
-        return userAuthCode = ExitCodeType.INVALID_ACCESS.getExitCode();
+        assertEquals(ExitCodeType.INVALID_ACTION.getExitCode(), dataValidator.isDateDsAndDeValid(userInputData));
     }
 
-    private int authorizeUser() throws SQLException {
-
-        authorizationService = new AuthorizationService();
+    @Test
+    public void accountingTestInvalidVolume() throws SQLException {
 
         UserInputData userInputData = new UserInputData()
-                .withUserInputRole("READ")
-                .withUserInputPathResource("a.b");
-        String userRole = userInputData.getUserInputRole();
-        String userPath = userInputData.getUserInputPathResource();
+                .withUserInputVol("xxx");
 
-        if(authorizationService.isUserAuthorization(userResourceDAO, userPath, userRole,
-                userAuthCode) == ExitCodeType.SUCCESS.getExitCode()){
+        assertEquals(ExitCodeType.INVALID_ACTION.getExitCode(), dataValidator.isVolumeValid(userInputData));
+    }
 
-            return userAuthorizationCode = ExitCodeType.SUCCESS.getExitCode();
-        }
+    @Test
+    public void accountingTestValidDates() throws SQLException {
 
-        return userAuthorizationCode = ExitCodeType.INVALID_ACCESS.getExitCode();
+        UserInputData userInputData = new UserInputData()
+                .withUserInputDs("2017-10-19")
+                .withUserInputDe("2017-10-19");
+
+        assertEquals(ExitCodeType.SUCCESS.getExitCode(), dataValidator.isDateDsAndDeValid(userInputData));
+    }
+
+    @Test
+    public void accountingTestValidVolume() throws SQLException {
+
+        UserInputData userInputData = new UserInputData()
+                .withUserInputVol("100");
+
+        assertEquals(ExitCodeType.SUCCESS.getExitCode(), dataValidator.isVolumeValid(userInputData));
     }
 }
