35648ebcb7d754631e09e42e7dcd9518e9435852
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-11-06 23:56:45 +0700

    Работа над R6. Работа над п. 5. Закрыт п. 5. В классе AuthorityServlet переопределён метод: service(req resp).

--- appServer/src/main/java/general/servlets/AuthorityServlet.java
+++ appServer/src/main/java/general/servlets/AuthorityServlet.java
@@ -1,4 +1,15 @@
 package general.servlets;
 
-public class AuthorityServlet {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AuthorityServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+    }
 }
