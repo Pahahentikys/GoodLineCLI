d0fc8df7309817b9a106c07fe72508880606fb81
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-11-06 23:54:19 +0700

    Работа над R6. Работа над п. 5. Закрыт п. 4. В классеL UserServlet переопределён метод: service(req resp).

--- appServer/src/main/java/general/servlets/UserServlet.java
+++ appServer/src/main/java/general/servlets/UserServlet.java
@@ -1,4 +1,15 @@
 package general.servlets;
 
-public class UserServlet {
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+
+public class UserServlet extends HttpServlet {
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        super.service(req, res);
+    }
 }
