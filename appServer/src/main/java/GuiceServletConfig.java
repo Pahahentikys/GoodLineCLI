9410d0ed525c0690801922f576decac1f85872dc
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-11-07 00:33:28 +0700

    Работа над R6. Выполнен п. 6. 3. 1, сконфигурирован путь до: UserServlet.

--- appServer/src/main/java/GuiceServletConfig.java
+++ appServer/src/main/java/GuiceServletConfig.java
@@ -1,2 +1,21 @@
-public class GuiceServletConfig {
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.servlet.GuiceServletContextListener;
+import com.google.inject.servlet.ServletModule;
+import general.servlets.UserServlet;
+
+public class GuiceServletConfig extends GuiceServletContextListener {
+
+    @Override
+    protected Injector getInjector() {
+        return Guice.createInjector(new ServletModule() {
+            @Override
+            protected void configureServlets() {
+
+                // Конфигурация пути до UserServlet.
+                serve("/ajax/user").with(UserServlet.class);
+
+            }
+        });
+    }
 }
