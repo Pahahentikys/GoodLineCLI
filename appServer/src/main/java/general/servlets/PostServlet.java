2308cf07375b858859c37aa8ce6fa2d755863abe
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-11-09 01:07:08 +0300

    Работа над R6. Работа над п. 7. 6. Добавлен инжект логгера в класс: PostServlet. .

--- appServer/src/main/java/general/servlets/PostServlet.java
+++ appServer/src/main/java/general/servlets/PostServlet.java
@@ -1,5 +1,8 @@
 package general.servlets;
 
+import inject.logger.InjectLogger;
+import org.apache.logging.log4j.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -7,10 +10,18 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class PostServlet extends HttpServlet {
+
+    @InjectLogger
+    Logger logger;
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        logger.debug("It's PostServlet and method doPost");
+
+        logger.debug("ID with it will redirected to getServlet (get?id)" + req.getParameter("id"));
+
         // Передача id в get-сервлет.
-        resp.sendRedirect("get?id="+req.getParameter("id"));
+        resp.sendRedirect("get?id=" + req.getParameter("id"));
     }
 }
