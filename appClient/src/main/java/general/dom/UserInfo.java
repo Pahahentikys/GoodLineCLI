db2a04a7909ec90a8a35aee3769bd880a8d76a5a
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-11-10 02:26:47 +0300

    Работа над R6. Начата работа над п. 8. 3. Геттеры и билдеры класса: UserInfo изменены на аннотации.

--- appClient/src/main/java/general/dom/UserInfo.java
+++ appClient/src/main/java/general/dom/UserInfo.java
@@ -1,41 +1,22 @@
 package general.dom;
 
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
 public class UserInfo {
 
+    @Getter
     private int userId;
+
+    @Getter
     private String userLogin;
+
+    @Getter
     private String userHashPassword;
+
+    @Getter
     private String userSalt;
-
-    public int getUserId() {
-        return userId;
-    }
-    public String getUserLogin() {
-        return userLogin;
-    }
-    public String getUserHashPassword() {
-        return userHashPassword;
-    }
-    public String getUserSalt() {
-        return userSalt;
-    }
-
-    public UserInfo withUserId(int userId) {
-        this.userId = userId;
-        return this;
-    }
-    public UserInfo withUserLogin(String userLogin) {
-        this.userLogin = userLogin;
-        return this;
-    }
-    public UserInfo withUserHashPassword(String userHashPassword) {
-        this.userHashPassword = userHashPassword;
-        return this;
-    }
-    public UserInfo withUserSalt(String userSalt) {
-        this.userSalt = userSalt;
-        return this;
-    }
 
 }
