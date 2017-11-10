754fac92efade3047bd20975ff29e9b3f30d10c9
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-11-10 18:35:02 +0300

    Работа над R6. Начата работа над п. 8. 3 (настройка работы с Lombock). В классе: UserInputData добавлены аннотации вместо геттеров, сеттеров и билдеров.

--- appClient/src/main/java/general/dom/UserInputData.java
+++ appClient/src/main/java/general/dom/UserInputData.java
@@ -1,118 +1,38 @@
 package general.dom;
 
+import lombok.*;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
 public class UserInputData {
 
-    int userInputId;
+    @Getter
+    @Setter
     private String userInputLogin;
+
+    @Getter
+    @Setter
     private String userInputPassword;
+
+    @Getter
+    @Setter
     private String userInputPathResource;
+
+    @Getter
+    @Setter
     private String userInputRole;
+
+    @Getter
+    @Setter
     private String userInputDs;
+
+    @Getter
+    @Setter
     private String userInputDe;
+
+    @Getter
+    @Setter
     private String userInputVol;
 
-    public int getUserInputId() {
-        return userInputId;
-    }
-
-    public String getUserInputLogin() {
-        return userInputLogin;
-    }
-
-    public String getUserInputPassword() {
-        return userInputPassword;
-    }
-
-    public String getUserInputPathResource() {
-        return userInputPathResource;
-    }
-
-    public String getUserInputRole() {
-        return userInputRole;
-    }
-
-    public String getUserInputDs() {
-        return userInputDs;
-    }
-
-    public String getUserInputDe() {
-        return userInputDe;
-    }
-
-    public String getUserInputVol() {
-        return userInputVol;
-    }
-
-    public UserInputData withUserInputId(int userInputId) {
-        this.userInputId = userInputId;
-        return this;
-    }
-
-    public UserInputData withUserInputLogin(String userInputLogin) {
-        this.userInputLogin = userInputLogin;
-        return this;
-    }
-
-    public UserInputData withUserInputPassword(String userInputPassword) {
-        this.userInputPassword = userInputPassword;
-        return this;
-    }
-
-    public UserInputData withUserInputPathResource(String userInputPathResource) {
-        this.userInputPathResource = userInputPathResource;
-        return this;
-    }
-
-    public UserInputData withUserInputRole(String userInputRole) {
-        this.userInputRole = userInputRole;
-        return this;
-    }
-
-    public UserInputData withUserInputDs(String userInputDs) {
-        this.userInputDs = userInputDs;
-        return this;
-
-    }
-
-    public UserInputData withUserInputDe(String userInputDe) {
-        this.userInputDe = userInputDe;
-        return this;
-    }
-
-    public UserInputData withUserInputVol(String userInputVol) {
-        this.userInputVol = userInputVol;
-        return this;
-    }
-
-    public UserInputData() {
-
-    }
-
-    public UserInputData(String userInputLogin, String userInputPassword) {
-        this.userInputLogin = userInputLogin;
-        this.userInputPassword = userInputPassword;
-    }
-
-    public UserInputData(String userInputLogin, String userInputPassword, String userInputRole, String userInputPathResource) {
-        this.userInputLogin = userInputLogin;
-        this.userInputPassword = userInputPassword;
-        this.userInputRole = userInputRole;
-        this.userInputPathResource = userInputPathResource;
-    }
-
-    public UserInputData(String userInputLogin, String userInputPassword, String userInputPathResource, String userInputRole, String userInputDs, String userInputDe, String userInputVol) {
-        this.userInputLogin = userInputLogin;
-        this.userInputPassword = userInputPassword;
-        this.userInputPathResource = userInputPathResource;
-        this.userInputRole = userInputRole;
-        this.userInputDs = userInputDs;
-        this.userInputDe = userInputDe;
-        this.userInputVol = userInputVol;
-    }
-
-    public UserInputData(String userInputDs, String userInputDe, String userInputVol) {
-        this.userInputDs = userInputDs;
-        this.userInputDe = userInputDe;
-        this.userInputVol = userInputVol;
-    }
 }
