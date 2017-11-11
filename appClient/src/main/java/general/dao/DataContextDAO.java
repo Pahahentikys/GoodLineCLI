6ab44cbb5864b3c3856434e69488a7672111da3c
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-11-11 23:45:22 +0700

    Работа над R6.  Работа над 8. 3. В классе: DataContextDAO поля переделаны на геттеры и билдеры при помощи аннотаций.

--- appClient/src/main/java/general/dao/DataContextDAO.java
+++ appClient/src/main/java/general/dao/DataContextDAO.java
@@ -1,47 +1,29 @@
 package general.dao;
 
+import lombok.Builder;
+import lombok.Getter;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+@Builder
 public class DataContextDAO {
 
+    @Getter
     private String dataBaseDriver;
+
+    @Getter
     private String dataBaseUrl;
+
+    @Getter
     private String dataBaseUserName;
+
+    @Getter
     private String dataBasePassword;
 
-    public String getDataBaseDriver() {
-        return dataBaseDriver;
-    }
-    public String getDataBaseUrl() {
-        return dataBaseUrl;
-    }
-    public String getDataBaseUserName() {
-        return dataBaseUserName;
-    }
-    public String getDataBasePassword() {
-        return dataBasePassword;
-    }
     public Connection getConnection() throws SQLException, ClassNotFoundException {
         return DriverManager.getConnection(dataBaseUrl, dataBaseUserName, dataBasePassword);
-    }
-
-    public DataContextDAO withDataBaseDriver(String dataBaseDriver) {
-        this.dataBaseDriver = dataBaseDriver;
-        return this;
-    }
-    public DataContextDAO withDataBaseUrl(String dataBaseUrl) {
-        this.dataBaseUrl = dataBaseUrl;
-        return this;
-    }
-    public DataContextDAO withDataBaseUserName(String dataBaseUserName) {
-        this.dataBaseUserName = dataBaseUserName;
-        return this;
-    }
-    public DataContextDAO withDataBasePassword(String dataBasePassword) {
-        this.dataBasePassword = dataBasePassword;
-        return this;
     }
 
 }
