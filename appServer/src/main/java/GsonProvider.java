9caa98596d28dbb8ffc09de7b771fc8e3bbe2d9a
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-11-15 00:49:24 +0300

    Работа над R7. Выполнен п. 5. 1. Добавлена реализация к классу: GsonProvider в методе: get().

--- appServer/src/main/java/GsonProvider.java
+++ appServer/src/main/java/GsonProvider.java
@@ -1,9 +1,10 @@
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.inject.Provider;
 
 public class GsonProvider implements Provider<Gson> {
     @Override
     public Gson get() {
-        return null;
+        return new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
     }
 }
