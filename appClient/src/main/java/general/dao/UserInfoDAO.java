85b59864a61a87799d232f6b8e908b0e7686cca5
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-11-17 00:51:21 +0700

    Работа над R7. Работа над п. 9. В класс: UserInfoDAO добавлен метод: getAllUsersInfo(), который позволяет выбрать всех пользователей.

--- appClient/src/main/java/general/dao/UserInfoDAO.java
+++ appClient/src/main/java/general/dao/UserInfoDAO.java
@@ -8,17 +8,50 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserInfoDAO {
 
     private static final Logger logger = LogManager.getLogger(UserInfoDAO.class.getName());
 
-    public static final String SELECT_WHERE_USER_LOGIN = "SELECT * FROM USERS WHERE USER_LOGIN = ?";
+    private static final String SELECT_WHERE_USER_LOGIN = "SELECT * FROM USERS WHERE USER_LOGIN = ?";
+
+    private static final String SELECT_ALL_USERS = "SELECT * FROM USERS";
 
     private Connection connection;
 
     public UserInfoDAO(Connection connection) {
         this.connection = connection;
+    }
+
+    public List<UserInfo> getAllUsersInfo() {
+        logger.debug("Готовим запрос: " + SELECT_ALL_USERS);
+
+        List<UserInfo> userInfos = new ArrayList<>();
+
+        try {
+            PreparedStatement statement = connection.prepareStatement(SELECT_ALL_USERS);
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                logger.debug("Запрос выполнен. Наполнение данными объекта UserInfo");
+                userInfos.add(
+                        UserInfo.builder()
+                                .userId(resultSet.getInt("USER_ID"))
+                                .userLogin(resultSet.getString("USER_LOGIN"))
+                                .userHashPassword(resultSet.getString("USER_PASS_HASH"))
+                                .userSalt(resultSet.getString("USER_SALT"))
+                                .build());
+            }
+
+            return userInfos;
+
+        } catch (SQLException e) {
+            logger.error("Ошибка доступа к БД, приложение не работает!", e);
+        }
+
+        return null;
     }
 
     public UserInfo searchUserLogin(String userLogin) throws SQLException {
