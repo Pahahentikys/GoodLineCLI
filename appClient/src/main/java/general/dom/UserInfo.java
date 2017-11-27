9d5b0470fc1e04bd2bb8cb82b3073529e04e2334
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-11-28 04:00:45 +0700

    Работа над R7. Работа над п. 9. 1. Фикс работы сервлета: UserServlet и запроса на получение всех пользователей. В классе: UserInfo необходимые сериализуемые поля  пометил аннотацией: @Expose. Добавлена также аннотация: @SerializedName.

--- appClient/src/main/java/general/dom/UserInfo.java
+++ appClient/src/main/java/general/dom/UserInfo.java
@@ -1,15 +1,21 @@
 package general.dom;
 
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 import lombok.Builder;
 import lombok.Getter;
 
 @Builder
 public class UserInfo {
 
+    @SerializedName("user_id")
+    @Expose
     @Getter
     private int userId;
 
+    @SerializedName("user_login")
+    @Expose
     @Getter
     private String userLogin;
 
