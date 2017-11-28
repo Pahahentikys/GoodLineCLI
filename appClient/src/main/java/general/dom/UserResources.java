c18108aec392f2d7f1a7296490ca2d793241b0dd
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-11-29 01:54:54 +0700

    Работа над R7. Работа над п. 9. 2. В классе: UserResources помечены  необходимые сериализуемые поля через аннотацию: @Expose, также через аннотацию: @SerializedName помечено название поля, которое будет в JSON-объекте.

--- appClient/src/main/java/general/dom/UserResources.java
+++ appClient/src/main/java/general/dom/UserResources.java
@@ -1,20 +1,28 @@
 package general.dom;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 import lombok.Builder;
 import lombok.Getter;
 
 @Builder
 public class UserResources {
 
+    @SerializedName("user_role")
+    @Expose
     @Getter
     private UserRoles userRole;
 
+    @SerializedName("resource_path")
+    @Expose
     @Getter
     private String resourcePath;
 
     @Getter
     private int userResId;
 
+    @SerializedName("user_id")
+    @Expose
     @Getter
     private int userResUserId;
 
