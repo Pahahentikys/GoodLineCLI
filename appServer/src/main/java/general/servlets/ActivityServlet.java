b2a66750d52d9eca0273a6280cc62f4c0792ac8f
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-12-11 01:41:22 +0700

    Работа над R7. Выполнен п. 9. 3. В классе: ActivityServlet реализован метод на получение и вывод всех пользовательских сеансов.

--- appServer/src/main/java/general/servlets/ActivityServlet.java
+++ appServer/src/main/java/general/servlets/ActivityServlet.java
@@ -1,6 +1,8 @@
 package general.servlets;
 
 import com.google.gson.Gson;
+import general.dao.AccountingDAO;
+import general.dom.Accounting;
 import inject.logger.InjectLogger;
 import org.apache.logging.log4j.Logger;
 
@@ -11,6 +13,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @Singleton
 public class ActivityServlet extends HttpServlet {
@@ -21,10 +24,25 @@ public class ActivityServlet extends HttpServlet {
     @Inject
     Gson gson;
 
+    private String jsonResp;
+
+    @Inject
+    private AccountingDAO accountingDAO;
+
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.service(req, resp);
 
         logger.debug("It's ActivityServlet!");
+
+        if(req.getParameter("id")==null){
+            logger.debug("ID сеанса не получен, выводится весь список сеансов!");
+            List<Accounting> accountings = accountingDAO.getAllUserSeanses();
+            logger.debug("Запрос на вывод всех сеансов пошёл!");
+            jsonResp = gson.toJson(accountings);
+            logger.debug("Сеансы: {}", jsonResp );
+        }
+
+        resp.setContentType("application/json");
+        resp.getWriter().write(jsonResp);
     }
 }
