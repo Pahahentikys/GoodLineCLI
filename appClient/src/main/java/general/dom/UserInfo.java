fca1dbb5b7e4b2d59ce2632245da788e71306a24
Pahahentikys <Pavel1995.rus@yandex.ru>
2018-01-04 23:16:53 +0300

    Работа над R8. Подредактировал п. 5. Реализовал п. 5. 2. Класс: UserInfo подстроен под работу с ORM, добавлены необходимые аннотации.

--- appClient/src/main/java/general/dom/UserInfo.java
+++ appClient/src/main/java/general/dom/UserInfo.java
@@ -6,22 +6,32 @@ import com.google.gson.annotations.SerializedName;
 import lombok.Builder;
 import lombok.Getter;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user_info")
 @Builder
 public class UserInfo {
 
+    @Id
+    @GeneratedValue
+    @Column(name = "user_id")
     @SerializedName("user_id")
     @Expose
     @Getter
     private int userId;
 
+    @Column(name="user_login")
     @SerializedName("user_login")
     @Expose
     @Getter
     private String userLogin;
 
+    @Column(name = "user_hash_password")
     @Getter
     private String userHashPassword;
 
+    @Column(name = "user_salt")
     @Getter
     private String userSalt;
 
