8710a221106983bd263b7470ad036aa92c9eb566
Pahahentikys <Pavel1995.rus@yandex.ru>
2018-01-04 23:32:53 +0300

    Работа над R8. Подредактировал п. 5. Реализовал п. 5 полностью. Класс: UserResources подстроен под работу с ORM, добавлены необходимые аннотации.

--- appClient/src/main/java/general/dom/UserResources.java
+++ appClient/src/main/java/general/dom/UserResources.java
@@ -4,15 +4,33 @@ import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user_resources")
 @Builder
 public class UserResources {
 
+    @Column(name="user_resources_id")
+    @Id
+    @GeneratedValue
+    @Getter
+    @Setter
+    private int userResourcesId;
+
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private UserInfo userInfo;
+
+    @Column(name="user_role")
     @SerializedName("user_role")
     @Expose
     @Getter
     private UserRoles userRole;
 
+    @Column(name="resource_path")
     @SerializedName("resource_path")
     @Expose
     @Getter
@@ -21,6 +39,7 @@ public class UserResources {
     @Getter
     private int userResId;
 
+    @Column(name="user_res_user_id")
     @SerializedName("user_id")
     @Expose
     @Getter
