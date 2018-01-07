da14e2e8c4afba9685e9fb4cd99d836f9939e6ba
Pahahentikys <Pavel1995.rus@yandex.ru>
2018-01-07 22:05:36 +0300

    Работа над R8. Работа над п. 6 закончена (выполнены п. 6. 1, 6. 3). Добавил работу с переменными окружения через System.getenv().

--- appServer/src/main/java/providers/EntityManagerProvider.java
+++ appServer/src/main/java/providers/EntityManagerProvider.java
@@ -5,6 +5,10 @@ import general.dao.DataContextDAO;
 import org.flywaydb.core.Flyway;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.HashMap;
+import java.util.Map;
 
 public class EntityManagerProvider implements Provider<EntityManager> {
 
@@ -21,10 +25,19 @@ public class EntityManagerProvider implements Provider<EntityManager> {
                 .dataBasePassword("1234")
                 .build();
 
-
         Flyway flyway = new Flyway();
         flyway.setDataSource(dataContextDAO.getDataBaseUrl(), dataContextDAO.getDataBaseUserName(), dataContextDAO.getDataBasePassword());
         flyway.migrate();
+
+        Map<String, String> env = System.getenv();
+        Map<String, String> properties = new HashMap<>();
+
+        properties.put("javax.persistence.jdbc.url", env.get("JDBC_DATABASE_URL"));
+        properties.put("javax.persistence.jdbc.user", env.get("JDBC_DATABASE_USERNAME"));
+        properties.put("javax.persistence.jdbc.password", env.get("JDBC_DATABASE_PASSWORD"));
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("GoodLineCLI", properties);
+        entityManager = entityManagerFactory.createEntityManager();
 
     }
 
