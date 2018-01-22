10220382374cbe5d9a44baeb8563bcda18b79e76
Pahahentikys <Pavel1995.rus@yandex.ru>
2018-01-22 20:42:39 +0300

    Работа над  8. 1. Попытка правки запуска миграций на Heroku. Поправил свойства в persistence.xml, в EMprovider реализовано получение данных подключения к БД через переменные окружения.

--- appServer/src/main/java/providers/EntityManagerProvider.java
+++ appServer/src/main/java/providers/EntityManagerProvider.java
@@ -19,19 +19,25 @@ public class EntityManagerProvider implements Provider<EntityManager> {
 
     public EntityManagerProvider() {
 
+        Map<String, String> env = System.getenv();
+
         DataContextDAO dataContextDAO;
         dataContextDAO = DataContextDAO.builder()
                 .dataBaseDriver("org.postgresql.Driver")
-                .dataBaseUrl("jdbc:h2:./src/main/resources/db/GoodLineCLIPostgre;MODE=PostgreSQL")
-                .dataBaseUserName("Pavel")
-                .dataBasePassword("1234")
+//                .dataBaseUrl("jdbc:h2:./src/main/resources/db/GoodLineCLIPostgre3;MODE=PostgreSQL")
+//                .dataBaseUserName("Pavel")
+//                .dataBasePassword("1234")
+                .dataBaseUrl(env.get("JDBC_DATABASE_URL"))
+                .dataBaseUserName(env.get("JDBC_DATABASE_USERNAME"))
+                .dataBasePassword(env.get("JDBC_DATABASE_PASSWORD"))
+
                 .build();
+
 
         Flyway flyway = new Flyway();
         flyway.setDataSource(dataContextDAO.getDataBaseUrl(), dataContextDAO.getDataBaseUserName(), dataContextDAO.getDataBasePassword());
         flyway.migrate();
 
-        Map<String, String> env = System.getenv();
         Map<String, String> properties = new HashMap<>();
 
         properties.put("javax.persistence.jdbc.url", env.get("JDBC_DATABASE_URL"));
