38db910cd5c26448c10f8251e9276135c5331fca
Pahahentikys <Pavel1995.rus@yandex.ru>
2018-05-26 03:40:28 +0700

    Работа над  8. 1. Починка деплоя на Heroku, добавил дополнительное поле в DataContextDAO

--- appClient/src/main/java/general/dao/DataContextDAO.java
+++ appClient/src/main/java/general/dao/DataContextDAO.java
@@ -1,13 +1,14 @@
 package general.dao;
 
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 @Builder
+//@NoArgsConstructor(access = AccessLevel.PUBLIC)
+//@AllArgsConstructor(access = AccessLevel.PUBLIC)
 public class DataContextDAO {
 
     @Getter
@@ -21,6 +22,8 @@ public class DataContextDAO {
 
     @Getter
     private String dataBasePassword;
+
+    private String dataBaseDialect;
 
     public Connection getConnection() throws SQLException, ClassNotFoundException {
         return DriverManager.getConnection(dataBaseUrl, dataBaseUserName, dataBasePassword);
